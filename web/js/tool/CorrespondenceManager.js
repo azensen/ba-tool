86a8f199bf1ce21c8ec184569289bea81494b8a2
azensen <andre.zensen@googlemail.com>
2016-06-27 18:43:11 +0200

    corrManager almost ready! needs functions for specific transformations

--- web/js/tool/CorrespondenceManager.js
+++ web/js/tool/CorrespondenceManager.js
@@ -3,34 +3,58 @@
  */
 "use strict";
 
-var CorrespondenceManager = function() {
+function CorrespondenceManager () {
     this.correspondenceList = [];
     this.association = {};
 };
 
-CorrespondenceManager.prototype.loadAssociations = function (filePath) {
+CorrespondenceManager.prototype.loadAssociation = function (filePath) {
+    var assoFile = filePath.substr(0, filePath.lastIndexOf(".")) + ".json";
+    console.log(assoFile);
+    //$.get(assoFile, _this.parseAssociationFile, 'text');
+/*    $.getJSON(filePath, function (data) {
+        console.log("Success getJSON");
+        console.log(data);
+    });*/
     var _this = this;
-    var assoFile = filePath.substr(0, filePath.lastIndexOf(".")) + ".asso";
-    $.get(assoFile, _this.parseAssociationFile, 'text');
+    $.getJSON(assoFile, function(data) {
+        console.log("Successfully loaded asso-JSON:");
+        console.log(data);
+        _this.setAssociation(data);
+        })
+            .error(function(xhr) {
+                console.log(xhr);
+                console.log(Object.keys(xhr));
+            });
 }
 
-CorrespondenceManager.parseAssociationFile = function (assoContent) {
-    var assoContent = assoContent;
-    var parsedContent = $.parseJSON(assoContent);
-    this.association = parsedContent;
-};
+CorrespondenceManager.prototype.setAssociation = function (association) {
+    this.association = association;
+}
 
 CorrespondenceManager.prototype.loadCorrespondences = function (filePath) {
-    var _this = this;
     var corrFile = filePath.substr(0, filePath.lastIndexOf(".")) + ".corr";
-    $.get(corrFile, _this.parseCorrespondenceFile, 'text');
+    console.log(corrFile);
+    //$.get(assoFile, _this.parseAssociationFile, 'text');
+    /*    $.getJSON(filePath, function (data) {
+     console.log("Success getJSON");
+     console.log(data);
+     });*/
+    var _this = this;
+    $.getJSON(corrFile, function(data) {
+        console.log("Successfully loaded corr-JSON:");
+        console.log(data);
+        _this.setCorrespondences(data);
+    })
+        .error(function(xhr) {
+            console.log(xhr);
+            console.log(Object.keys(xhr));
+        });
 }
 
-CorrespondenceManager.parseCorrespondenceFile = function (corrContent) {
-    var corrContent = corrContent;
-    var parsedContent = $.parseJSON(corrContent);
-    this.correspondenceList = parsedContent;
-};
+CorrespondenceManager.prototype.setCorrespondences = function (data) {
+    this.correspondenceList = data;
+}
 
 CorrespondenceManager.prototype.addCorr = function (corr) {
     this.corrList.push(corr);
@@ -99,7 +123,77 @@ CorrespondenceManager.prototype.getCorr = function (elementID) {
             }
         }
     }
-    return corr = new Correspondence("not_found", "not_found", "not_found");
+    return corr = new Correspondence("not_found", "", "");
+};
+
+CorrespondenceManager.prototype.getCorrBySource = function (elementID) {
+    var corrList = this.correspondenceList;
+    var corr;
+    for (var i = 0; i < corrList.length; i++) {
+        corr = corrList[i];
+        var corrType = corr.corrType;
+        //var n = str1.localeCompare(str2);
+        if(corrType == "oneone") {
+            if(corr.source == elementID) {
+                return corr;
+            }
+        } else if (corrType == "onezero") {
+            if(corr.source == elementID) {
+                return corr;
+            }
+        } else if (corrType == "zeroone") {
+            continue;
+        } else if (corrType == "onemany") {
+            if(corr.source == elementID) {
+                return corr;
+            } else {
+                continue;
+            }
+        } else if (corrType == "manyone") {
+            for (var k = 0; k < corr.source.length; k++) {
+                if(corr.source[k] == elementID) {
+                    return corr;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+    return corr = new Correspondence("not_found", "", "");
+};
+
+CorrespondenceManager.prototype.getCorrByTarget = function (elementID) {
+    var corrList = this.correspondenceList;
+    var corr;
+    for (var i = 0; i < corrList.length; i++) {
+        corr = corrList[i];
+        var corrType = corr.corrType;
+        //var n = str1.localeCompare(str2);
+        if (corrType == "oneone") {
+            if (corr.target == elementID) {
+                return corr;
+            }
+        } else if (corrType == "onezero") {
+            continue;
+        } else if (corrType == "zeroone") {
+            if (corr.target == elementID) {
+                return corr;
+            }
+        } else if (corrType == "onemany") {
+            for (var j = 0; j < corr.target.length; j++) {
+                if (corr.target[j] == elementID) {
+                    return corr;
+                }
+            }
+        } else if (corrType == "manyone") {
+            if (corr.target == elementID) {
+                return corr;
+            } else {
+                continue;
+            }
+        }
+    }
+    return corr = new Correspondence("not_found", "", "");
 };
 /*
  Structure of correspondence types
